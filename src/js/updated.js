a1bddf6d47ab35c561de5fd6519dd2dd087fd0af
skjongithub <elgoognojks@gmail.com>
2026-03-29 12:48:10 +0800

    Delete src directory

--- src/js/updated.js
+++
@@ -1,9 +0,0 @@
-
-var whiteList, option;
-
-jQuery(document).ready(function () {
-  localizePage();
-  setVersionInfo();
-  $("#showOptions").click(function () { showOptions(); return false; });
-});
-
--- src/readme.txt
+++
@@ -1,5 +0,0 @@
-VANILLA
-Chrome Cookie Whitelist Manager
-by CoralLlama@gmail.com
-
-Thanks to http://code.google.com/p/adblockforchrome/ for the options page!
